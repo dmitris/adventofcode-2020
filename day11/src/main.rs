ecb680b5ff198df0b9423eff47d1f1fcbd595bcf
Dmitri S <dmitris@users.noreply.github.com>
2020-12-17 21:54:39 +0100

    day11 WIP

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,16 @@
+use std::io::BufRead;
+
+enum SeatType (
+    Occupied
+    Empty
+    Floor
+);
+
+struct Rec
 fn main() {
+    let recs = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line2rec(line)).collect();
     println!("Hello, world!");
 }
